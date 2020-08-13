62d077b52f15a3829c90e0aa79a13df25fdfce95
emipa606 <emil.offermo@litet.net>
2020-08-13 10:48:56 +0200

    First publish

---
+++ Source/NPCBonus/NPCBonusUtility.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using RimWorld;
+using Verse;
+
+namespace NPCBonus
+{
+	// Token: 0x02000003 RID: 3
+	public class NPCBonusUtility
+	{
+		// Token: 0x06000004 RID: 4 RVA: 0x00002084 File Offset: 0x00000284
+		public static float GetMinWeaponValue(PawnKindDef PKD)
+		{
+			float min = PKD.weaponMoney.min;
+			float num = PKD.weaponMoney.max;
+			if (((PKD != null) ? PKD.weaponTags : null) == null || (PKD != null && PKD.weaponTags.Count == 0))
+			{
+				return min;
+			}
+			List<ThingStuffPair> list = new List<ThingStuffPair>();
+			if (NPCBonusUtility.allWeaponPairs.Count > 0)
+			{
+				list = NPCBonusUtility.allWeaponPairs;
+			}
+			else
+			{
+				list = ThingStuffPair.AllWith((ThingDef t) => t.IsWeapon);
+			}
+			if (list.Count > 0)
+			{
+				NPCBonusUtility.allWeaponPairs = list;
+			}
+			if (((PKD != null) ? PKD.defaultFactionType : null) != null)
+			{
+				TechLevel minCompareTechLevel = NPCBonusUtility.GetMinCompareTechLevel(PKD.defaultFactionType.techLevel);
+				float num2 = 999999f;
+				float num3;
+				float num4;
+				float num5;
+				if (NPCBonusUtility.GetAdjVal(minCompareTechLevel, out num3, out num4, out num5) && num4 != 0f)
+				{
+					num = Math.Max(num * ((num4 + 100f) / 100f), 0f);
+				}
+				if (num > 0f)
+				{
+					num2 = num;
+					foreach (ThingStuffPair thingStuffPair in NPCBonusUtility.allWeaponPairs)
+					{
+						if (thingStuffPair.thing.techLevel == minCompareTechLevel && num2 > thingStuffPair.Price)
+						{
+							num2 = thingStuffPair.Price;
+						}
+					}
+					if (num2 <= num && num2 > min)
+					{
+						return num2;
+					}
+				}
+			}
+			return min;
+		}
+
+		// Token: 0x06000005 RID: 5 RVA: 0x000021F0 File Offset: 0x000003F0
+		private static TechLevel GetMinCompareTechLevel(TechLevel TL)
+		{
+			if (TL == TechLevel.Neolithic || TL == TechLevel.Medieval)
+			{
+				return TechLevel.Neolithic;
+			}
+			if (TL == TechLevel.Industrial)
+			{
+				return TechLevel.Industrial;
+			}
+			if (TL == TechLevel.Spacer || TL == TechLevel.Ultra || TL == TechLevel.Archotech)
+			{
+				return TechLevel.Spacer;
+			}
+			return TechLevel.Neolithic;
+		}
+
+		// Token: 0x06000006 RID: 6 RVA: 0x00002214 File Offset: 0x00000414
+		internal static bool GetAdjVal(TechLevel tech, out float app, out float wpn, out float tch)
+		{
+			bool result = false;
+			app = (wpn = (tch = 0f));
+			switch (tech)
+			{
+			case TechLevel.Neolithic:
+				result = true;
+				app = Settings.PreAppPct;
+				wpn = Settings.PreWpnPct;
+				tch = Settings.PreTchPct;
+				break;
+			case TechLevel.Medieval:
+				result = true;
+				app = Settings.PreAppPct;
+				wpn = Settings.PreWpnPct;
+				tch = Settings.PreTchPct;
+				break;
+			case TechLevel.Industrial:
+				result = true;
+				app = Settings.AppPct;
+				wpn = Settings.WpnPct;
+				tch = Settings.TchPct;
+				break;
+			case TechLevel.Spacer:
+				result = true;
+				app = Settings.PostAppPct;
+				wpn = Settings.PostWpnPct;
+				tch = Settings.PostTchPct;
+				break;
+			case TechLevel.Ultra:
+				result = true;
+				app = Settings.PostAppPct;
+				wpn = Settings.PostWpnPct;
+				tch = Settings.PostTchPct;
+				break;
+			case TechLevel.Archotech:
+				result = true;
+				app = Settings.PostAppPct;
+				wpn = Settings.PostWpnPct;
+				tch = Settings.PostTchPct;
+				break;
+			}
+			return result;
+		}
+
+		// Token: 0x04000002 RID: 2
+		private static List<ThingStuffPair> allWeaponPairs = new List<ThingStuffPair>();
+	}
+}
---
+++ Source/NPCBonus/Settings.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using UnityEngine;
+using Verse;
+
+namespace NPCBonus
+{
+	// Token: 0x02000005 RID: 5
+	public class Settings : ModSettings
+	{
+		// Token: 0x0600000B RID: 11 RVA: 0x000025EC File Offset: 0x000007EC
+		public void DoWindowContents(Rect canvas)
+		{
+			Listing_Standard listing_Standard = new Listing_Standard();
+			listing_Standard.ColumnWidth = canvas.width;
+			listing_Standard.Begin(canvas);
+			float num = 2f;
+			listing_Standard.Gap(num);
+			float max = 100f;
+			float min = -10f;
+			checked
+			{
+				listing_Standard.Label("NPCBonus.PreAppPct".Translate() + "  " + (int)Settings.PreAppPct, -1f, null);
+				Settings.PreAppPct = (float)((int)listing_Standard.Slider((float)((int)Settings.PreAppPct), min, max));
+				listing_Standard.Gap(num);
+				listing_Standard.Label("NPCBonus.PreWpnPct".Translate() + "  " + (int)Settings.PreWpnPct, -1f, null);
+				Settings.PreWpnPct = (float)((int)listing_Standard.Slider((float)((int)Settings.PreWpnPct), min, max));
+				listing_Standard.Gap(num);
+				listing_Standard.Label("NPCBonus.PreTchPct".Translate() + "  " + (int)Settings.PreTchPct, -1f, null);
+				Settings.PreTchPct = (float)((int)listing_Standard.Slider((float)((int)Settings.PreTchPct), min, max));
+				listing_Standard.Gap(num);
+				listing_Standard.Label("NPCBonus.AppPct".Translate() + "  " + (int)Settings.AppPct, -1f, null);
+				Settings.AppPct = (float)((int)listing_Standard.Slider((float)((int)Settings.AppPct), min, max));
+				listing_Standard.Gap(num);
+				listing_Standard.Label("NPCBonus.WpnPct".Translate() + "  " + (int)Settings.WpnPct, -1f, null);
+				Settings.WpnPct = (float)((int)listing_Standard.Slider((float)((int)Settings.WpnPct), min, max));
+				listing_Standard.Gap(num);
+				listing_Standard.Label("NPCBonus.TchPct".Translate() + "  " + (int)Settings.TchPct, -1f, null);
+				Settings.TchPct = (float)((int)listing_Standard.Slider((float)((int)Settings.TchPct), min, max));
+				listing_Standard.Gap(num);
+				listing_Standard.Label("NPCBonus.PostAppPct".Translate() + "  " + (int)Settings.PostAppPct, -1f, null);
+				Settings.PostAppPct = (float)((int)listing_Standard.Slider((float)((int)Settings.PostAppPct), min, max));
+				listing_Standard.Gap(num);
+				listing_Standard.Label("NPCBonus.PostWpnPct".Translate() + "  " + (int)Settings.PostWpnPct, -1f, null);
+				Settings.PostWpnPct = (float)((int)listing_Standard.Slider((float)((int)Settings.PostWpnPct), min, max));
+				listing_Standard.Gap(num);
+				listing_Standard.Label("NPCBonus.PostTchPct".Translate() + "  " + (int)Settings.PostTchPct, -1f, null);
+				Settings.PostTchPct = (float)((int)listing_Standard.Slider((float)((int)Settings.PostTchPct), min, max));
+				listing_Standard.Gap(num);
+				listing_Standard.CheckboxLabeled("NPCBonus.AffordWeapon".Translate(), ref Settings.AffordWeapon, null);
+			}
+			listing_Standard.Gap(num * 2f);
+			Text.Font = GameFont.Tiny;
+			listing_Standard.Label("          " + "NPCBonus.Tip".Translate(), -1f, null);
+			Text.Font = GameFont.Small;
+			listing_Standard.Gap(num);
+			listing_Standard.End();
+		}
+
+		// Token: 0x0600000C RID: 12 RVA: 0x00002978 File Offset: 0x00000B78
+		public override void ExposeData()
+		{
+			base.ExposeData();
+			Scribe_Values.Look<float>(ref Settings.PreAppPct, "PreAppPct", 0f, false);
+			Scribe_Values.Look<float>(ref Settings.PreWpnPct, "PreWpnPct", 0f, false);
+			Scribe_Values.Look<float>(ref Settings.PreTchPct, "PreTchPct", 0f, false);
+			Scribe_Values.Look<float>(ref Settings.AppPct, "AppPct", 0f, false);
+			Scribe_Values.Look<float>(ref Settings.WpnPct, "WpnPct", 0f, false);
+			Scribe_Values.Look<float>(ref Settings.TchPct, "TchPct", 0f, false);
+			Scribe_Values.Look<float>(ref Settings.PostAppPct, "PostAppPct", 0f, false);
+			Scribe_Values.Look<float>(ref Settings.PostWpnPct, "PostWpnPct", 0f, false);
+			Scribe_Values.Look<float>(ref Settings.PostTchPct, "PostTchPct", 0f, false);
+			Scribe_Values.Look<bool>(ref Settings.AffordWeapon, "AffordWeapon", false, false);
+		}
+
+		// Token: 0x04000003 RID: 3
+		public static float PreAppPct;
+
+		// Token: 0x04000004 RID: 4
+		public static float PreWpnPct;
+
+		// Token: 0x04000005 RID: 5
+		public static float PreTchPct;
+
+		// Token: 0x04000006 RID: 6
+		public static float AppPct;
+
+		// Token: 0x04000007 RID: 7
+		public static float WpnPct;
+
+		// Token: 0x04000008 RID: 8
+		public static float TchPct;
+
+		// Token: 0x04000009 RID: 9
+		public static float PostAppPct;
+
+		// Token: 0x0400000A RID: 10
+		public static float PostWpnPct;
+
+		// Token: 0x0400000B RID: 11
+		public static float PostTchPct;
+
+		// Token: 0x0400000C RID: 12
+		public static bool AffordWeapon;
+	}
+}
