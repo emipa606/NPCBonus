30f6807e39a605d70ec16faff196e7aba9fa22d7
emipa606 <emil.offermo@litet.net>
2021-07-10 22:56:12 +0200

    Mod updated for 1.3 and passed autotests

--- Source/NPCBonus/NPCBonus_Initializer.cs
+++ Source/NPCBonus/NPCBonus_Initializer.cs
@@ -1,104 +1,121 @@
 ﻿using System;
-using System.Collections.Generic;
 using Verse;
 
 namespace NPCBonus
 {
-	// Token: 0x02000004 RID: 4
-	[StaticConstructorOnStartup]
-	internal static class NPCBonus_Initializer
-	{
-		// Token: 0x06000009 RID: 9 RVA: 0x00002302 File Offset: 0x00000502
-		static NPCBonus_Initializer()
-		{
-			LongEventHandler.QueueLongEvent(new Action(NPCBonus_Initializer.Setup), "LibraryStartup", false, null, true);
-		}
+    // Token: 0x02000004 RID: 4
+    [StaticConstructorOnStartup]
+    internal static class NPCBonus_Initializer
+    {
+        // Token: 0x06000009 RID: 9 RVA: 0x00002302 File Offset: 0x00000502
+        static NPCBonus_Initializer()
+        {
+            LongEventHandler.QueueLongEvent(Setup, "LibraryStartup", false, null);
+        }
 
-		// Token: 0x0600000A RID: 10 RVA: 0x00002320 File Offset: 0x00000520
-		public static void Setup()
-		{
-			List<PawnKindDef> allDefsListForReading = DefDatabase<PawnKindDef>.AllDefsListForReading;
-			if (allDefsListForReading.Count > 0)
-			{
-				int num = 0;
-				int num2 = 0;
-				int num3 = 0;
-				foreach (PawnKindDef pawnKindDef in allDefsListForReading)
-				{
-					float num4;
-					float num5;
-					float num6;
-					if (((pawnKindDef != null) ? pawnKindDef.defaultFactionType : null) != null && !pawnKindDef.defaultFactionType.isPlayer && NPCBonusUtility.GetAdjVal(pawnKindDef.defaultFactionType.techLevel, out num4, out num5, out num6))
-					{
-						if (num4 != 0f)
-						{
-							pawnKindDef.apparelMoney.min = Math.Max(pawnKindDef.apparelMoney.min * ((num4 + 100f) / 100f), 0f);
-							pawnKindDef.apparelMoney.max = Math.Max(pawnKindDef.apparelMoney.max * ((num4 + 100f) / 100f), 0f);
-							checked
-							{
-								num++;
-							}
-						}
-						if (num5 != 0f || Settings.AffordWeapon)
-						{
-							float num7 = 0f;
-							bool flag = false;
-							bool flag2 = false;
-							if (Settings.AffordWeapon)
-							{
-								num7 = NPCBonusUtility.GetMinWeaponValue(pawnKindDef);
-							}
-							float num8 = Math.Max(pawnKindDef.weaponMoney.min * ((num5 + 100f) / 100f), 0f);
-							float num9;
-							if (Settings.AffordWeapon)
-							{
-								num9 = ((num7 >= num8) ? num7 : num8);
-							}
-							else
-							{
-								num9 = num8;
-							}
-							if (num9 != pawnKindDef.weaponMoney.min)
-							{
-								pawnKindDef.weaponMoney.min = num9;
-								flag = true;
-							}
-							float num10 = Math.Max(pawnKindDef.weaponMoney.max * ((num5 + 100f) / 100f), 0f);
-							float num11;
-							if (Settings.AffordWeapon)
-							{
-								num11 = ((num10 >= num9) ? num10 : num9);
-							}
-							else
-							{
-								num11 = num10;
-							}
-							if (num11 != pawnKindDef.weaponMoney.max)
-							{
-								pawnKindDef.weaponMoney.max = num11;
-								flag2 = true;
-							}
-							checked
-							{
-								if (flag || flag2)
-								{
-									num2++;
-								}
-							}
-						}
-						if (num6 != 0f)
-						{
-							pawnKindDef.techHediffsMoney.min = Math.Max(pawnKindDef.techHediffsMoney.min * ((num6 + 100f) / 100f), 0f);
-							pawnKindDef.techHediffsMoney.max = Math.Max(pawnKindDef.techHediffsMoney.max * ((num6 + 100f) / 100f), 0f);
-							checked
-							{
-								num3++;
-							}
-						}
-					}
-				}
-				Log.Message("NPCBonus.Report".Translate(num.ToString(), num2.ToString(), num3.ToString()), false);
-			}
-		}
-	}
+        // Token: 0x0600000A RID: 10 RVA: 0x00002320 File Offset: 0x00000520
+        public static void Setup()
+        {
+            var allDefsListForReading = DefDatabase<PawnKindDef>.AllDefsListForReading;
+            if (allDefsListForReading.Count <= 0)
+            {
+                return;
+            }
+
+            var num = 0;
+            var num2 = 0;
+            var num3 = 0;
+            foreach (var pawnKindDef in allDefsListForReading)
+            {
+                if (pawnKindDef?.defaultFactionType == null ||
+                    pawnKindDef.defaultFactionType.isPlayer || !NPCBonusUtility.GetAdjVal(
+                        pawnKindDef.defaultFactionType.techLevel, out var num4, out var num5,
+                        out var num6))
+                {
+                    continue;
+                }
+
+                if (num4 != 0f)
+                {
+                    pawnKindDef.apparelMoney.min =
+                        Math.Max(pawnKindDef.apparelMoney.min * ((num4 + 100f) / 100f), 0f);
+                    pawnKindDef.apparelMoney.max =
+                        Math.Max(pawnKindDef.apparelMoney.max * ((num4 + 100f) / 100f), 0f);
+                    checked
+                    {
+                        num++;
+                    }
+                }
+
+                if (num5 != 0f || Settings.AffordWeapon)
+                {
+                    var num7 = 0f;
+                    var moneyMin = false;
+                    var moneyMax = false;
+                    if (Settings.AffordWeapon)
+                    {
+                        num7 = NPCBonusUtility.GetMinWeaponValue(pawnKindDef);
+                    }
+
+                    var num8 = Math.Max(pawnKindDef.weaponMoney.min * ((num5 + 100f) / 100f), 0f);
+                    float num9;
+                    if (Settings.AffordWeapon)
+                    {
+                        num9 = num7 >= num8 ? num7 : num8;
+                    }
+                    else
+                    {
+                        num9 = num8;
+                    }
+
+                    if (num9 != pawnKindDef.weaponMoney.min)
+                    {
+                        pawnKindDef.weaponMoney.min = num9;
+                        moneyMin = true;
+                    }
+
+                    var num10 = Math.Max(pawnKindDef.weaponMoney.max * ((num5 + 100f) / 100f), 0f);
+                    float num11;
+                    if (Settings.AffordWeapon)
+                    {
+                        num11 = num10 >= num9 ? num10 : num9;
+                    }
+                    else
+                    {
+                        num11 = num10;
+                    }
+
+                    if (num11 != pawnKindDef.weaponMoney.max)
+                    {
+                        pawnKindDef.weaponMoney.max = num11;
+                        moneyMax = true;
+                    }
+
+                    checked
+                    {
+                        if (moneyMin || moneyMax)
+                        {
+                            num2++;
+                        }
+                    }
+                }
+
+                if (num6 == 0f)
+                {
+                    continue;
+                }
+
+                pawnKindDef.techHediffsMoney.min =
+                    Math.Max(pawnKindDef.techHediffsMoney.min * ((num6 + 100f) / 100f), 0f);
+                pawnKindDef.techHediffsMoney.max =
+                    Math.Max(pawnKindDef.techHediffsMoney.max * ((num6 + 100f) / 100f), 0f);
+                checked
+                {
+                    num3++;
+                }
+            }
+
+            Log.Message("NPCBonus.Report".Translate(num.ToString(), num2.ToString(), num3.ToString()));
+        }
+    }
 }
--- Source/NPCBonus/Settings.cs
+++ Source/NPCBonus/Settings.cs
@@ -1,105 +1,104 @@
-﻿using System;
-using UnityEngine;
+﻿using UnityEngine;
 using Verse;
 
 namespace NPCBonus
 {
-	// Token: 0x02000005 RID: 5
-	public class Settings : ModSettings
-	{
-		// Token: 0x0600000B RID: 11 RVA: 0x000025EC File Offset: 0x000007EC
-		public void DoWindowContents(Rect canvas)
-		{
-			Listing_Standard listing_Standard = new Listing_Standard();
-			listing_Standard.ColumnWidth = canvas.width;
-			listing_Standard.Begin(canvas);
-			float num = 2f;
-			listing_Standard.Gap(num);
-			float max = 100f;
-			float min = -10f;
-			checked
-			{
-				listing_Standard.Label("NPCBonus.PreAppPct".Translate() + "  " + (int)Settings.PreAppPct, -1f, null);
-				Settings.PreAppPct = (float)((int)listing_Standard.Slider((float)((int)Settings.PreAppPct), min, max));
-				listing_Standard.Gap(num);
-				listing_Standard.Label("NPCBonus.PreWpnPct".Translate() + "  " + (int)Settings.PreWpnPct, -1f, null);
-				Settings.PreWpnPct = (float)((int)listing_Standard.Slider((float)((int)Settings.PreWpnPct), min, max));
-				listing_Standard.Gap(num);
-				listing_Standard.Label("NPCBonus.PreTchPct".Translate() + "  " + (int)Settings.PreTchPct, -1f, null);
-				Settings.PreTchPct = (float)((int)listing_Standard.Slider((float)((int)Settings.PreTchPct), min, max));
-				listing_Standard.Gap(num);
-				listing_Standard.Label("NPCBonus.AppPct".Translate() + "  " + (int)Settings.AppPct, -1f, null);
-				Settings.AppPct = (float)((int)listing_Standard.Slider((float)((int)Settings.AppPct), min, max));
-				listing_Standard.Gap(num);
-				listing_Standard.Label("NPCBonus.WpnPct".Translate() + "  " + (int)Settings.WpnPct, -1f, null);
-				Settings.WpnPct = (float)((int)listing_Standard.Slider((float)((int)Settings.WpnPct), min, max));
-				listing_Standard.Gap(num);
-				listing_Standard.Label("NPCBonus.TchPct".Translate() + "  " + (int)Settings.TchPct, -1f, null);
-				Settings.TchPct = (float)((int)listing_Standard.Slider((float)((int)Settings.TchPct), min, max));
-				listing_Standard.Gap(num);
-				listing_Standard.Label("NPCBonus.PostAppPct".Translate() + "  " + (int)Settings.PostAppPct, -1f, null);
-				Settings.PostAppPct = (float)((int)listing_Standard.Slider((float)((int)Settings.PostAppPct), min, max));
-				listing_Standard.Gap(num);
-				listing_Standard.Label("NPCBonus.PostWpnPct".Translate() + "  " + (int)Settings.PostWpnPct, -1f, null);
-				Settings.PostWpnPct = (float)((int)listing_Standard.Slider((float)((int)Settings.PostWpnPct), min, max));
-				listing_Standard.Gap(num);
-				listing_Standard.Label("NPCBonus.PostTchPct".Translate() + "  " + (int)Settings.PostTchPct, -1f, null);
-				Settings.PostTchPct = (float)((int)listing_Standard.Slider((float)((int)Settings.PostTchPct), min, max));
-				listing_Standard.Gap(num);
-				listing_Standard.CheckboxLabeled("NPCBonus.AffordWeapon".Translate(), ref Settings.AffordWeapon, null);
-			}
-			listing_Standard.Gap(num * 2f);
-			Text.Font = GameFont.Tiny;
-			listing_Standard.Label("          " + "NPCBonus.Tip".Translate(), -1f, null);
-			Text.Font = GameFont.Small;
-			listing_Standard.Gap(num);
-			listing_Standard.End();
-		}
+    // Token: 0x02000005 RID: 5
+    public class Settings : ModSettings
+    {
+        // Token: 0x04000003 RID: 3
+        public static float PreAppPct;
 
-		// Token: 0x0600000C RID: 12 RVA: 0x00002978 File Offset: 0x00000B78
-		public override void ExposeData()
-		{
-			base.ExposeData();
-			Scribe_Values.Look<float>(ref Settings.PreAppPct, "PreAppPct", 0f, false);
-			Scribe_Values.Look<float>(ref Settings.PreWpnPct, "PreWpnPct", 0f, false);
-			Scribe_Values.Look<float>(ref Settings.PreTchPct, "PreTchPct", 0f, false);
-			Scribe_Values.Look<float>(ref Settings.AppPct, "AppPct", 0f, false);
-			Scribe_Values.Look<float>(ref Settings.WpnPct, "WpnPct", 0f, false);
-			Scribe_Values.Look<float>(ref Settings.TchPct, "TchPct", 0f, false);
-			Scribe_Values.Look<float>(ref Settings.PostAppPct, "PostAppPct", 0f, false);
-			Scribe_Values.Look<float>(ref Settings.PostWpnPct, "PostWpnPct", 0f, false);
-			Scribe_Values.Look<float>(ref Settings.PostTchPct, "PostTchPct", 0f, false);
-			Scribe_Values.Look<bool>(ref Settings.AffordWeapon, "AffordWeapon", false, false);
-		}
+        // Token: 0x04000004 RID: 4
+        public static float PreWpnPct;
 
-		// Token: 0x04000003 RID: 3
-		public static float PreAppPct;
+        // Token: 0x04000005 RID: 5
+        public static float PreTchPct;
 
-		// Token: 0x04000004 RID: 4
-		public static float PreWpnPct;
+        // Token: 0x04000006 RID: 6
+        public static float AppPct;
 
-		// Token: 0x04000005 RID: 5
-		public static float PreTchPct;
+        // Token: 0x04000007 RID: 7
+        public static float WpnPct;
 
-		// Token: 0x04000006 RID: 6
-		public static float AppPct;
+        // Token: 0x04000008 RID: 8
+        public static float TchPct;
 
-		// Token: 0x04000007 RID: 7
-		public static float WpnPct;
+        // Token: 0x04000009 RID: 9
+        public static float PostAppPct;
 
-		// Token: 0x04000008 RID: 8
-		public static float TchPct;
+        // Token: 0x0400000A RID: 10
+        public static float PostWpnPct;
 
-		// Token: 0x04000009 RID: 9
-		public static float PostAppPct;
+        // Token: 0x0400000B RID: 11
+        public static float PostTchPct;
 
-		// Token: 0x0400000A RID: 10
-		public static float PostWpnPct;
+        // Token: 0x0400000C RID: 12
+        public static bool AffordWeapon;
 
-		// Token: 0x0400000B RID: 11
-		public static float PostTchPct;
+        // Token: 0x0600000B RID: 11 RVA: 0x000025EC File Offset: 0x000007EC
+        public void DoWindowContents(Rect canvas)
+        {
+            var listing_Standard = new Listing_Standard {ColumnWidth = canvas.width};
+            listing_Standard.Begin(canvas);
+            var num = 2f;
+            listing_Standard.Gap(num);
+            var max = 100f;
+            var min = -10f;
+            checked
+            {
+                listing_Standard.Label("NPCBonus.PreAppPct".Translate() + "  " + (int) PreAppPct);
+                PreAppPct = (int) listing_Standard.Slider((int) PreAppPct, min, max);
+                listing_Standard.Gap(num);
+                listing_Standard.Label("NPCBonus.PreWpnPct".Translate() + "  " + (int) PreWpnPct);
+                PreWpnPct = (int) listing_Standard.Slider((int) PreWpnPct, min, max);
+                listing_Standard.Gap(num);
+                listing_Standard.Label("NPCBonus.PreTchPct".Translate() + "  " + (int) PreTchPct);
+                PreTchPct = (int) listing_Standard.Slider((int) PreTchPct, min, max);
+                listing_Standard.Gap(num);
+                listing_Standard.Label("NPCBonus.AppPct".Translate() + "  " + (int) AppPct);
+                AppPct = (int) listing_Standard.Slider((int) AppPct, min, max);
+                listing_Standard.Gap(num);
+                listing_Standard.Label("NPCBonus.WpnPct".Translate() + "  " + (int) WpnPct);
+                WpnPct = (int) listing_Standard.Slider((int) WpnPct, min, max);
+                listing_Standard.Gap(num);
+                listing_Standard.Label("NPCBonus.TchPct".Translate() + "  " + (int) TchPct);
+                TchPct = (int) listing_Standard.Slider((int) TchPct, min, max);
+                listing_Standard.Gap(num);
+                listing_Standard.Label("NPCBonus.PostAppPct".Translate() + "  " + (int) PostAppPct);
+                PostAppPct = (int) listing_Standard.Slider((int) PostAppPct, min, max);
+                listing_Standard.Gap(num);
+                listing_Standard.Label("NPCBonus.PostWpnPct".Translate() + "  " + (int) PostWpnPct);
+                PostWpnPct = (int) listing_Standard.Slider((int) PostWpnPct, min, max);
+                listing_Standard.Gap(num);
+                listing_Standard.Label("NPCBonus.PostTchPct".Translate() + "  " + (int) PostTchPct);
+                PostTchPct = (int) listing_Standard.Slider((int) PostTchPct, min, max);
+                listing_Standard.Gap(num);
+                listing_Standard.CheckboxLabeled("NPCBonus.AffordWeapon".Translate(), ref AffordWeapon);
+            }
 
-		// Token: 0x0400000C RID: 12
-		public static bool AffordWeapon;
-	}
+            listing_Standard.Gap(num * 2f);
+            Text.Font = GameFont.Tiny;
+            listing_Standard.Label("          " + "NPCBonus.Tip".Translate());
+            Text.Font = GameFont.Small;
+            listing_Standard.Gap(num);
+            listing_Standard.End();
+        }
+
+        // Token: 0x0600000C RID: 12 RVA: 0x00002978 File Offset: 0x00000B78
+        public override void ExposeData()
+        {
+            base.ExposeData();
+            Scribe_Values.Look(ref PreAppPct, "PreAppPct");
+            Scribe_Values.Look(ref PreWpnPct, "PreWpnPct");
+            Scribe_Values.Look(ref PreTchPct, "PreTchPct");
+            Scribe_Values.Look(ref AppPct, "AppPct");
+            Scribe_Values.Look(ref WpnPct, "WpnPct");
+            Scribe_Values.Look(ref TchPct, "TchPct");
+            Scribe_Values.Look(ref PostAppPct, "PostAppPct");
+            Scribe_Values.Look(ref PostWpnPct, "PostWpnPct");
+            Scribe_Values.Look(ref PostTchPct, "PostTchPct");
+            Scribe_Values.Look(ref AffordWeapon, "AffordWeapon");
+        }
+    }
 }
